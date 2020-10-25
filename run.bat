ef653f439692f0a5c0b74f04a6f1933e91715ce5
Willard Kang <weig.kang@gmail.com>
2020-10-25 13:42:24 +0800

    Modify run.bat

--- run.bat
+++ run.bat
@@ -1,5 +1,5 @@
 %~d0
 cd %~dp0
 cd "venv\scripts"
-activate & pip install -r requirements.txt & cd "..\.." & payslip_sender.py
+activate & cd "..\.." & pip install -r requirements.txt & payslip_sender.py
 pause
